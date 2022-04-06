58d1df43dd0136bb14c8b38249be5ca6f2e12bb6
Yue Li <yl819@duke.edu>
2022-04-05 21:57:32 -0400

    filter orders by product and seller name

--- app/models/purchase.py
+++ app/models/purchase.py
@@ -31,10 +31,6 @@ ORDER BY time DESC
             uid=uid, since=since)
         return [Purchase(*row) for row in rows]
   
-  
-  
-
-
 class OrderDetail:
     def __init__(self, pid, productname, sid, sellerlastname, sellerfirstname,  quantity, final_unitprice, fulfilled):
         self.pid = pid
@@ -58,6 +54,34 @@ SELECT purchases.pid, products.name, purchases.sid, users.lastname, users.firstn
 FROM purchases, products, users
 WHERE purchases.id=:orderid
 AND products.id=purchases.pid
-AND users.id=purchases.uid
+AND users.id=purchases.sid
 ''',        orderid=orderid)
         return uidRow[0][0], [OrderDetail(*row) for row in orderRows]
+
+class FilteredItem:
+    def __init__(self,orderid, time_purchased, pid, productname, sid, sellerlastname, sellerfirstname, quantity, final_unitprice, fulfilled):
+        self.orderid = orderid
+        self.time_purchased = time_purchased
+        self.pid = pid
+        self.productname = productname
+        self.sid = sid
+        self.sellerlastname = sellerlastname
+        self.sellerfirstname = sellerfirstname
+        self.quantity = quantity
+        self.final_unitprice = final_unitprice
+        self.fulfilled = fulfilled
+    
+    @staticmethod
+    def getFilteredItem(productNameKeyword, sellerLastNameKeyword, sellerFirstNameKeyword, uid):
+        rows = app.db.execute(f'''
+SELECT purchases.id, purchases.time_purchased, purchases.pid, products.name, purchases.sid, users.lastname, users.firstname, purchases.quantity, purchases.finalprice, purchases.fulfillstate
+FROM purchases, products, users
+WHERE purchases.uid=:uid
+AND products.id=purchases.pid
+AND users.id=purchases.sid
+AND products.name LIKE '{"%"+productNameKeyword+"%"}'
+AND users.lastname LIKE '{"%"+sellerLastNameKeyword+"%"}'
+AND users.firstname LIKE '{"%"+sellerFirstNameKeyword+"%"}'
+ORDER BY purchases.time_purchased DESC
+''', uid=uid)
+        return  [FilteredItem(*row) for row in rows]
